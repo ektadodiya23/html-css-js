6dc9a4ba43c9cb57fab40f57ff5143efe11cf8d3
Ekta Dodiya <Ekta.Dodiya@MSBC-MAINFRAME.LCL>
2023-02-13 13:11:02 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+function myfunction(){
+    document.getElementById("language").style.display = 'flex';
+    document.getElementById("population").style.display = 'none';
+}
+
+function myfunction2(){
+    document.getElementById("language").style.display = 'none';
+    document.getElementById("population").style.display = 'flex';
+ }
+
+
+
